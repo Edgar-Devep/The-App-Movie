3529d37c28c225e0457210f3620133a0c3d7dd5d
Edgar Delgado <edgardelgado3007@gmail.com>
2026-03-07 02:00:23 +0000

    revisado y modificado

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,4 @@
-import {
-  BrowserRouter,
-  Outlet,
-  Route,
-  Routes,
-  useLocation,
-} from "react-router-dom";
+import { BrowserRouter, Outlet, Route, Routes, useLocation } from "react-router-dom";
 import { ComingSoon } from "./components/Coming_Soon";
 import { Footer } from "./UI/Footer";
 import { TrendingMovie } from "./components/Trending_Movie";
@@ -17,6 +11,7 @@ import { Nav } from "./UI/Nav";
 import { CategoryMovies } from "./components/Category";
 import { Popular } from "./components/Popular";
 import { CategoryForMovie } from "./components/Category_List_Movie";
+import { My_List_Like_Movie } from "./components/My_List_Like_Movie";
 
 function Layout() {
   const location = useLocation();
@@ -24,10 +19,11 @@ function Layout() {
 
   return (
     <div className="w-full min-h-screen bg-linear-to-b from-secondary via-primary-slow-800 to-secondary">
+      
       {isHomePage ? (
         <div className="relative">
           <PosterPathPrincipal />
-          <LogoMovie overlay />
+          <LogoMovie />
           <Nav />
         </div>
       ) : (
@@ -54,16 +50,33 @@ function App() {
               index
               element={
                 <>
-                  <TrendingMovie /> <ComingSoon /> <Popular />  <CategoryMovies />
+                  <TrendingMovie key="trending_home" /> <ComingSoon />{" "}
+                  <Popular />
+                  <CategoryMovies />
                 </>
               }
             />
-            <Route path="/trending" element={<TrendingMovie />} />
-            <Route path="/coming-soon" element={<ComingSoon />} />
-            <Route path="/popular" element={<Popular />} />
+            <Route
+              path="trending"
+              element={<TrendingMovie key="trending_page" />}
+            />
+            <Route path="coming-soon" element={<ComingSoon />} />
+            <Route path="popular" element={<Popular />} />
             <Route path="search" element={<SearchMovie />} />
-            <Route path="/categories/:name/:id" element={<CategoryForMovie />} />
-            <Route path="*" element={<NotFound />} />
+            <Route path="categories/:name/:id" element={<CategoryForMovie />} />
+            <Route path="my_list" element={<My_List_Like_Movie />} />
+            <Route
+              path="*"
+              element={
+                <NotFound
+                  title="La pagina que buscas no existe"
+                  src="/error_404.png"
+                  alt="Error 404"
+                  error="Error 404"
+                  showBtn={true}
+                />
+              }
+            />
           </Route>
         </Routes>
       </BrowserRouter>
