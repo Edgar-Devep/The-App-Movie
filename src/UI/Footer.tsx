cbb385b3da5ac00529f157178d0d48f91fcb20f4
Edgar Delgado <edgardelgado3007@gmail.com>
2026-04-06 15:32:14 +0000

    agregando enlace al footer

--- src/UI/Footer.tsx
+++ src/UI/Footer.tsx
@@ -2,9 +2,11 @@ export const Footer = () => {
   return (
     <>
       <footer className="w-full flex justify-center pb-14 pt-4 md:pt-8">
-        <h2 className="inline-flex text-xs md:text-3xl text-white hover:text-blue-500 hover:drop-shadow-[3px_7px_6px_#FFF]">
-          Made with 💓 @rivasedgar3007
-        </h2>
+        <a href="https://www.linkedin.com/in/edgar-francisco-delgado-rivas-32526a253/" target="_blank" rel="noopener noreferrer">
+          <h2 className="inline-flex text-xs md:text-3xl text-white hover:text-blue-500 hover:drop-shadow-[3px_7px_6px_#FFF]">
+            Made with 💓 Edgar Delgado
+          </h2>
+        </a>
       </footer>      
     </>
   );
